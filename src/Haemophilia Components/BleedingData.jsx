be5f97f87b8bd954d646daf75902b77740e0690d
MaccccD <Dumisanimofolo@gmail.com>
2025-05-23 23:58:21 +0200

    Clotting Levels Data Update Part 1
    -Added some clotting data.
    -Added blood clots to bleeding data.

--- src/Haemophilia Components/BleedingData.jsx
+++ src/Haemophilia Components/BleedingData.jsx
@@ -5,6 +5,7 @@ function BleedingData() {
   //State variables for the different typesof bleeding data :
   const [bleedingCount, setBleedingCount] = useState(0);
   const [intensity, setIntensity] = useState("");
+  const [bloodClot, setBloodClot] = useState("");
   const [site, setSite] = useState("");
   const [duration, setDuration] = useState("");
   const [trigger, setTrigger] = useState("");
@@ -15,10 +16,11 @@ function BleedingData() {
   const handleSave =()=>{
     if (!notes || !intensity || !site || !duration || !trigger || !treatment) {
     alert("Please fill in all required fields before saving.");
-    return; // ensuring that the entr does not save unless all the fields have been filled.
+    return; // ensuring that the blood episode entry entry does not save unless all the fields have been filled.
     }
     const bleedingLog = {
       bleedingCount,
+      bloodClot,
       intensity,
       site,
       duration,
@@ -56,8 +58,15 @@ function BleedingData() {
        <button onClick={()=> setBleedingCount(prev => Math.min(prev + 1, 100))} className='nextBtn'>+</button>
        <button onClick={()=> setBleedingCount(prev=> Math.max(prev -1, 0))} className='nextBtn'>-</button>
 
+       <p className='content'>2. How long does it take for your blood to clot when your bleeding ?</p>
+       <select className='setUp' onChange={(b)=> setBloodClot(b.target.value)} value={bloodClot}>
+        <option value= "5 minutes">25 - 30 seconds</option>
+        <option value= "25 muntes">45 seconds</option>
+        <option value= "45 minutes">60 seconds</option>
+       </select>
 
-       <p className='content'>2.  Bleeding Intensity:</p>
+
+       <p className='content'>3.  Bleeding Intensity:</p>
        <select className='setUp' onChange={(e) => setIntensity(e.target.value)} value={intensity}>
         <option value="">Select Intensity below:</option>
         <option value= "Light">Light</option>
@@ -65,22 +74,22 @@ function BleedingData() {
         <option value="Severe">Severe</option>
        </select>
 
-       <p className='content'>3. Bleeding Site:</p>
+       <p className='content'>4. Bleeding Site:</p>
        <input type='text' className='setUp' placeholder='e.g: Nose, Joint, Ears, etc.' value={site} onChange={(e)=> setSite(e.target.value)} required/>
 
-       <p className='content'>4. Bleeding Duration (minutes):</p>
+       <p className='content'>5. Bleeding Duration (minutes):</p>
        <input type='number' min= "0" className='setUp' value={duration} onChange={(d)=> setDuration(d.target.value)} required/>
 
-       <p className='content'>5. What triggered it?:</p>
+       <p className='content'>6. What triggered it?:</p>
        <input type='text' placeholder='spontaneous,physical activity etc.' className='setUp' value={trigger} onChange={(t)=> setTrigger(t.target.value)} required/>
 
-       <p className='content'>6. Treatment taken?:</p>
+       <p className='content'>7. Treatment taken?:</p>
        <input type='text' className='setUp' placeholder= "e.g: Factor VIII"value={treatment} onChange={(e)=>setTreatment(e.target.value)} required/>
 
-       <p className='content'>7. Pain Level (1-10):</p>
+       <p className='content'>8. Pain Level (1-10):</p>
        <input type='range' className='setUp' min="1" max= "10" value={painLevel} onChange={(e)=> setPainLevel(e.target.value)} required/>
 
-       <p className='content'>8. Additional Notes:</p>
+       <p className='content'>9. Additional Notes:</p>
        <textarea value={notes}   className= "setUp" onChange={(e)=> setNotes(e.target.value)} rows= "4" required/>
 
         <br /><br />
--- src/Haemophilia Components/ClottingLevels.jsx
+++ src/Haemophilia Components/ClottingLevels.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function ClottingLevels() {
+  const [clottingLevels, setClottingLevels] = useState([]);
+   const [latestBloodClot, setLatestBloodClot]= useState(null);
+
+   //factors affecting clotting levels:
+   const [age, setAge] = useState(0);
+   const [sex, setSex] = useState("");
+   const [genetics, setGenetics] = useState("");
+   const [medicalCondition, setMedicalCondition] = useState("");
+   const [medications, setMedications] = useState("");
+   const [diet, setDiet] = useState("");
+
+
+    useEffect(()=>{
+      const storedBleedLogs = JSON.parse(localStorage.getItem("bleedingLogs") || "[]");
+      if(storedBleedLogs.length > 0){
+       const lastBloodClotEntry = storedBleedLogs[storedBleedLogs.length - 1] //getting the selctedoption form the drop down
+       const clottingPeriod = lastBloodClotEntry.bloodClot;
+       setLatestBloodClot(clottingPeriod);
+   
+        }
+     }, []);
+   
   return (
     <div>
-      <p className='content'>Here are your clotting levels:</p>
+      <p className='content'>Based on your bleeding data, it took {latestBloodClot} for your blood to clot.</p>
+      <p className='content'> Please fill in the following fields that also affect your blood clotting ability to generate a specific coagulation test:</p>
+      <p className='content'> 1. How old are you ?:</p>
+      <input type='number' min= "0" className='setUp' value={age} onChange={(d)=> setAge(d.target.value)} required/>
+
+      <p className='content'>2. Please select your sexual orientiation:</p>
+      <select value={sex} className='setUp' onChange={(s)=> setSex(s.target.value)}>
+        <option value= "Male">Male</option>
+        <option value= "Female">Female</option>
+      </select>
+
+      <p className='content'>3. Please select a genetic condition(s) you have (if any):</p>
+      <input className='setUp' placeholder='Conditions like Fibrinogen deficiency, von Willebrand disease, Haemophilia, Factor V Leiden thrombophilia etc' onChange={(g)=> setGenetics(g.target.value)}/>
+      <select value={genetics} className='setUp' onChange={(g)=> setGenetics(g.target.value)}>
+        <option value= "Fibrinogen deficiency">Fibrinogen deficiency</option>
+        <option value= "Von Willebrand disease">Von Willebrand disease</option>
+      </select>
+
+
+      <p>4. Set other Medical Condition(s) you have (if any)</p>
+
+    
+
     </div>
   )
 }
