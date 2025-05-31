b3289cb9f3c940a9d6e2e29db7cbdac1e7a39975
MaccccD <Dumisanimofolo@gmail.com>
2025-05-31 23:43:14 +0200

    Diabetes Data Functionality Update Part 1
    -Added the data to log for diabetes.

--- src/Diabetes Components/Diabetes.jsx
+++ src/Diabetes Components/Diabetes.jsx
@@ -2,6 +2,17 @@ import React, { useState } from 'react'
 
 function Diabetes() {
   const [diabetesType, setDiabetesType] = useState("");
+  const [glucoseLevel, setGlucoseLevel] = useState(0);
+  const [insulinType, setInsulinType] = useState("");
+  const [insulinDose, setInsulinDose] = useState(0);
+  const [waterIntake, setWaterIntake] = useState("");
+  const [carbsIntake, setCarbsIntake] = useState(0);
+  const [symptoms, setSymptoms] = useState("");
+  const [mood, setMood] = useState("");
+  const [phsyicalActivity, setPhyicalActvity] = useState("");
+
+
+
 
   return (
     <div className='diabetes-Container'>
@@ -15,7 +26,24 @@ function Diabetes() {
       <option value= "Type 1">Type 1</option>
       <option value="Type 2">Type 2</option>
      </select>
-    </div>
+     <br/>
+
+     <label className='content'>2. What is your current glocose level ?</label>
+      <p className='content'>Blood Glucose Level: {`${glucoseLevel}`}</p>
+       <button onClick={()=> setGlucoseLevel(prev => Math.min(prev + 1, 100))} className='bleed-Count'>+</button>
+       <button onClick={()=> setGlucsoseLevel(prev=> Math.max(prev -1, 0))} className='bleed-Count'>-</button>
+       <br/>
+      <label className='content'>3. What type of insulin do you use ?</label>
+      <select value={insulinType} className='setUIp' onChange={(i)=> setInsulinType(i.target.value)}>
+        <option value= "">Select Option</option>
+        <option value= "rapid-acting">Rapid Acting</option>
+        <option value= "long-acting">Low Acting</option>
+      </select>
+      <br/>
+      <label className='content'>3. What is your average insulin dose daily ?</label><br/>
+      <input type='range' placeholder='Dosage (units)' value={insulinDose} onChange={(i)=> setInsulinDose(i.target.value)}/>
+
+      </div>
   )
 }
 
