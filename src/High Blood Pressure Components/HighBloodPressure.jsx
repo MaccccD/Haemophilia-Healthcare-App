93d1ef699c183f165a937d582ee39cc8c511583d
MaccccD <Dumisanimofolo@gmail.com>
2025-06-03 23:39:37 +0200

    High  Blood Pressure Functionality Update Part 1
    -Added first half of data to be logged in.

--- src/High Blood Pressure Components/HighBloodPressure.jsx
+++ src/High Blood Pressure Components/HighBloodPressure.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function HighBloodPressure() {
+
+  const [diet, setDiet] = useState([]);
+  const [phsyicalActivity, setPhysicalActivity] = useState("");
+  const [alcoholConsumption, setAlcoholConsumption] = useState();
+  const [tobaccoUse, setTobbacoUse] = useState("");
+  const [stressLevels, setStressLevels] = useState("");
+  const [weight, setWeight] = useState(0);
+ 
+
+  const dietOptions = [
+    "Exessive Salt Intake",
+    " High Saturated Fats & trans fats",
+    "Low Intake OF Fruits & vegetables",
+  ]
+
+
   return (
     <div className='bloodPressure-Container'>
-      <p className='content'>Please enter your blood pressure levels</p>
+      <h1 className='heading'>High Blood Pressure levels:</h1>
+      <p className='content'> Please log in your entry:</p>
+
+
+      <label className='content'>1. Please select your diet Intake ?</label><br/><br/>
+     <select value={diet} className='setUp' onChange={(t)=>setDiet(t.target.value)}>
+      <option value= "">Diabetes Options: </option>
+      <option value= "Type 1">Type 1</option>
+      <option value="Type 2">Type 2</option>
+     </select>
+     <br/><br/>
     </div>
   )
 }
